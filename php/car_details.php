0e87b24e180e2b46ac1b1dbeceacc2cc46002d45
David García <daganu@gmail.com>
2024-11-08 10:16:09 +0100

    user must be loggued

--- php/car_details.php
+++ php/car_details.php
@@ -1,4 +1,10 @@
 <?php session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+  header('Location: login.php');
+  exit;
+}
+
 include('header.php');
 include('db.php'); ?>
 
