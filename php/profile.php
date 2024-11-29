34f26c1517688e7b20d0e6077886f866a25f8e94
David García <daganu@gmail.com>
2024-11-29 19:26:39 +0100

    added url escape

--- php/profile.php
+++ php/profile.php
@@ -14,12 +14,12 @@ $query = $db->query('SELECT * FROM users WHERE username = "' . $username . '"');
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $newDescription = $_POST['description'];
-    $newWeather = $_POST['weather'];
-    $db->query('UPDATE users SET description = "' . $newDescription . '" WHERE username = "' . $username . '"');
-    $db->query('UPDATE users SET weather = "' . $newWeather . '" WHERE username = "' . $username . '"');
-    header("Location: success.php");
-    exit;
+  $newDescription = $_POST['description'];
+  $newWeather = $_POST['weather'];
+  $db->query('UPDATE users SET description = "' . $newDescription . '" WHERE username = "' . $username . '"');
+  $db->query('UPDATE users SET weather = "' . $newWeather . '" WHERE username = "' . $username . '"');
+  header("Location: success.php");
+  exit;
 }
 
 include('header.php');
@@ -31,7 +31,7 @@ if ($query->num_rows === 0) {
 }
 
 $user = $query->fetch_assoc();
-
+$weather = htmlspecialchars($user['weather'], ENT_QUOTES, 'UTF-8');
 
 ?>
 
@@ -52,14 +52,14 @@ $user = $query->fetch_assoc();
     </div>
     <div class="col-md-4">
       <div style="margin-top: 15px;"><strong>API Key:</strong> <?php echo $user['apikey']; ?></div>
-      <div style="margin-top: 15px; margin-bottom: 15px;"><strong>Weather URL:</strong><?php echo $user['weather']; ?></div>
+      <div style="margin-top: 15px; margin-bottom: 15px;"><strong>Weather URL:</strong><?php echo $weather; ?></div>
       <h5>Cambiar valores</h5>
       <form action="profile.php?username=<?php echo $username; ?>" method="post">
         <div class="form-group mb-3">
           <label>Descripción</label>
           <textarea class="form-control" id="description" name="description" required><?php echo $user['description']; ?></textarea>
           <label>Weather API Url</label>
-          <textarea class="form-control" id="weather" name="weather" required><?php echo $user['weather']; ?></textarea>
+          <textarea class="form-control" id="weather" name="weather" required><?php echo $weather; ?></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Guardar cambios</button>
       </form>
@@ -70,4 +70,3 @@ $user = $query->fetch_assoc();
 <?php
 include('footer.php');
 ?>
-
