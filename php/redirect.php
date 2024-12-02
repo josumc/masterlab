97e372563d892a4b343c58b46da605f4011aeee9
josumc <75732632+josumc@users.noreply.github.com>
2024-12-03 00:44:39 +0100

    Update redirect.php

--- php/redirect.php
+++ php/redirect.php
@@ -1,10 +1,24 @@
 <?php
+// Lista blanca de URLs permitidas para redirigir
+$allowed_urls = [
+    'index.php',
+    'profile.php',
+    'car_details.php'
+];
 
 if (isset($_GET['url'])) {
-  $url = $_GET['url'];
+    // Sanitizar y validar la URL
+    $url = filter_var($_GET['url'], FILTER_SANITIZE_URL);
 
-  header("Location: $url");
-  exit;
+    // Verificar si la URL es relativa y está en la lista blanca
+    if (in_array($url, $allowed_urls)) {
+        header("Location: $url");
+        exit;
+    } else {
+        // Si la URL no es válida o no está permitida, redirigir a una página segura
+        header("Location: index.php");
+        exit;
+    }
 } else {
-  echo "<p>No se proporcionó ninguna URL para redirigir.</p>";
+    echo "<p>No se proporcionó ninguna URL para redirigir.</p>";
 }
