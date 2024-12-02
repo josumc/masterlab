46c6510c6f42c29ab91c7e48ee4ae459c47baa5d
josumc <75732632+josumc@users.noreply.github.com>
2024-12-03 00:43:44 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -2,36 +2,47 @@
 session_start();
 
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-  header('Location: /');
-  exit;
+    header('Location: /');
+    exit;
 }
 
 include("db.php");
 include("serde.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $username = trim($_POST['username']);
-  $password = trim($_POST['password']);
+    // Sanitizar entrada
+    $username = trim(htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8'));
+    $password = trim($_POST['password']); // Contraseña no se escapa para preservar su formato
 
-  $conn = db_connect();
-  $sql = "SELECT id, pass, is_admin, weather FROM users WHERE username='$username'";
-  $result = $conn->query($sql);
+    $conn = db_connect();
 
-  if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    if (md5($password) === $row['pass']) {
-      $_SESSION['loggedin'] = true;
-      $_SESSION['username'] = $username;
+    // Usar consultas preparadas para evitar inyección SQL
+    $stmt = $conn->prepare("SELECT id, pass, is_admin, weather FROM users WHERE username = ?");
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-      /* $serinfo = ser($username, $row['is_admin'], $row['weather']); */
-      /* setcookie("user_info", $serinfo, time() + 36000, "/"); */
-      create_cookie($username);
-      header('Location: /');
-      exit();
+    if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+
+        // Verificar la contraseña con password_verify
+        if (password_verify($password, $row['pass'])) {
+            // Establecer la sesión
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+
+            // Generar el token CSRF para sesiones activas
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+
+            //Crear cookies
+            create_cookie($username);
+
+            header('Location: /');
+            exit();
+        }
     }
-  } else {
+
     $error = 'Usuario o contraseña incorrectos';
-  }
 }
 
 include('header.php');
@@ -43,7 +54,7 @@ include('header.php');
       <h2 class="text-center mb-4">Iniciar Sesión</h2>
       <?php if (isset($error)): ?>
         <div class="alert alert-danger" role="alert">
-          <?php echo $error; ?>
+          <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8'); ?>
         </div>
       <?php endif; ?>
       <form action="" method="post">
@@ -62,7 +73,6 @@ include('header.php');
   <div class="container mt-4 text-center">
     <p>¿No tienes una cuenta? <a href="register.php">Crea tu perfil aquí</a></p>
   </div>
-
 </div>
 
 <?php include('footer.php'); ?>
